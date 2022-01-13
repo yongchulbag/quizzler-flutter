57f464e345a15bf07bf8faaf82af97f32572ba21
flype <ggg>
2022-01-13 19:23:08 +0900

    notebook

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<widget> scoreKeeper =
+  [
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.check, color: Colors.green),
+  ]
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -85,6 +94,9 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+            children: scoreKeeper;
+        )
       ],
     );
   }
